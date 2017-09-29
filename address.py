ed0eca60278c6c3d3a62a5cc7d797febb7d59f0b
Shindo\Risa <risashindo729@gmail.com>
2017-09-29 12:24:54 +0800

    return distance value only

--- address.py
+++ address.py
@@ -1,26 +1,31 @@
 import requests
+import json
 
 def address(locationA, locationB):
 
-	locationA = 'Singapore ' + locationA
-	locationB = 'Singapore ' + locationB
+    locationA = 'Singapore ' + str(locationA)
+    locationB = 'Singapore ' + str(locationB)
 
-	token = 'AIzaSyDt1sdk_E9Xiauy2xb2V7otwS2qp-2xvfs'
+    token = 'AIzaSyDt1sdk_E9Xiauy2xb2V7otwS2qp-2xvfs'
 
-	api = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
+    api = 'https://maps.googleapis.com/maps/api/distancematrix/json?'
 
-	payload = {
-		'origins': locationA,
-		'destinations': locationB,
-		'mode': 'walking',
-		'key': token
-	}
+    payload = {
+            'origins': 'Singapore 550147',
+            'destinations': 'Singapore 822104',
+            'mode': 'walking',
+            'key': token
+            }
 
-	response = requests.get(api, params=payload)
+    response = requests.get(api, params=payload)
 
-	distance_result = response.text
+    result = response.text
+    json_data = json.loads(result)
+    distance_value = str(json_data["rows"][0]["elements"][0]["distance"]["text"])
+    distance_number = distance_value.split(' ', 1)[0]
+    print(distance_number)
 
-	return distance_result
+address(138609, 129588)
 
-	
+
 
